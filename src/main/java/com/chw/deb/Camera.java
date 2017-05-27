0a70e5c8c229442eaab392d07f7de214ab8b29ca
papillon <devopreet.ch@gmail.com>
2017-05-27 12:38:43 -0500

    added annotation support

--- src/main/java/com/chw/deb/Camera.java
+++ src/main/java/com/chw/deb/Camera.java
@@ -1,10 +1,12 @@
 package com.chw.deb;
 
+import org.springframework.stereotype.Component;
+
 /**
  * Created by papillon on 5/27/2017.
  */
+@Component
 public class Camera {
-
     public void snap(){
         System.out.println("SNAP!");
     }
--- src/main/java/com/chw/deb/Logger.java
+++ src/main/java/com/chw/deb/Logger.java
@@ -1,14 +1,27 @@
 package com.chw.deb;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by papillon on 5/27/2017.
  */
+@Component
+@Aspect
 public class Logger {
 
+    @Pointcut("execution(void com.chw.deb.Camera.snap())")
+    public void cameraSnap(){}
+
+    @Before("cameraSnap()")
     public void aboutToTakePhoto(){
         System.out.println("about to take photo....");
     }
 
+    @After("cameraSnap()")
     public void afterTakingPhoto(){
         System.out.println("photo taken....");
     }
