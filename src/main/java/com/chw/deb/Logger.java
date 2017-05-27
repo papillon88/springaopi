0cb7e31ab31d086c09801fb447454206a3b12880
papillon <devopreet.ch@gmail.com>
2017-05-27 11:35:39 -0500

    added aspect,pointcut and advice(before and after)

---
+++ src/main/java/com/chw/deb/Logger.java
@@ -0,0 +1,15 @@
+package com.chw.deb;
+
+/**
+ * Created by papillon on 5/27/2017.
+ */
+public class Logger {
+
+    public void aboutToTakePhoto(){
+        System.out.println("about to take photo....");
+    }
+
+    public void afterTakingPhoto(){
+        System.out.println("photo taken....");
+    }
+}
--- src/main/java/com/chw/deb/Main.java
+++ src/main/java/com/chw/deb/Main.java
@@ -1,7 +1,6 @@
 package com.chw.deb;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
