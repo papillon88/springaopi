888826da58b6c9616fa0f26bf04991dbc5944a9b
papillon <devopreet.ch@gmail.com>
2017-05-27 10:51:10 -0500

    using filexyxtemxmlappcon instead of classpathxmlappcon

--- src/main/java/com/chw/deb/Main.java
+++ src/main/java/com/chw/deb/Main.java
@@ -2,6 +2,7 @@ package com.chw.deb;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 /**
  * Created by papillon on 5/27/2017.
@@ -10,7 +11,7 @@ public class Main {
 
     public static void main(String[] args){
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("bean-config.xml");
+        ApplicationContext context = new FileSystemXmlApplicationContext("bean-config.xml");
         Camera cam = (Camera)context.getBean("camera");
         cam.snap();
     }
